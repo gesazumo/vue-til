ff2b4aaf53ba882a9d2ed380c871a5178a61c44c
teawoo <geaszumoo@gmailcom>
2021-07-13 15:17:48 +0900

    사이드 메뉴

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
 	<div>
-		<AppHeader />
-		<router-view />
+		<sidebar-menu :menu="menu" />
+		<router-view class="page" />
 	</div>
 </template>
 
 <script>
-import AppHeader from '@/components/common/AppHeader.vue'
+import { SidebarMenu } from 'vue-sidebar-menu'
 
 export default {
 	name: 'App',
 	components: {
-		AppHeader,
+		SidebarMenu,
+	},
+	data() {
+		return {
+			menu: [
+				{
+					header: true,
+					title: 'Main Navigation',
+					hiddenOnCollapse: true,
+				},
+				{
+					href: '/home',
+					title: 'Home',
+					icon: 'fa fa-user',
+				},
+				{
+					href: '/form',
+					title: 'FormPage',
+					icon: 'fa fa-chart-area',
+				},
+				{
+					href: '/vuex',
+					title: 'VuexPage',
+					icon: 'fa fa-user',
+				},
+				{
+					href: '/api',
+					title: 'ApiCallPage',
+					icon: 'fa fa-user',
+				},
+			],
+		}
 	},
 }
 </script>
 
-<style></style>
+<style>
+.page {
+	margin-left: 50px;
+	padding: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/routes/index'
 import vuetify from './plugins/vuetify'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 Vue.config.productionTip = false
 
